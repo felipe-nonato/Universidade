b97afb651018810c15533df8c997906217778573
lipe <felipe.tavares.nonato@gmail.com>
2022-05-26 00:59:02 -0300

    alteração head ques-4

--- codes/C/atividades/lista-1-av/ques-4/head.h
+++ codes/C/atividades/lista-1-av/ques-4/head.h
@@ -1,5 +1,7 @@
 // ------------ QUESTÃO 4 ------------
 
+/* 
+Recebe: Tamanho da string e a string em si. Retorna: void, printando se CPF/CNPJ é válido.
+*/
 void isValid(int tamanho, char str[tamanho]);
 
-int maior(int x, int y);
