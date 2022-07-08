ec140f5325873704b01ecc4cd47fec02436d6a84
Felipe Nonato <105125624+felipe-nonato@users.noreply.github.com>
2022-07-08 15:32:54 -0300

    questão 1 - prova anderson

---
+++ codes/C/provas/prova-1/questao_1.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Assinatura de função
+void recebeVetor(int l,int vetor[l][l]);
+void imprimeVetor(int l,int vetor[l][l]);
+int somaAbaixo(int l,int vetor[l][l]);
+int somaAcima(int l,int vetor[l][l]);
+
+int main()
+{   
+    // Declaração de variaveis
+    char posicao[6];
+    int limiar;
+    short int indice;
+    int soma;
+
+    // Recebendo variaveis
+
+        // recebendo posição
+            scanf("%s", posicao);
+        // recebendo limiar
+            scanf("%d",&limiar);
+        //recebendo matriz;
+            scanf("%hd", &indice);
+            int vetor[indice][indice];
+            recebeVetor(indice,vetor);
+            
+            
+            
+    
+    //verificando se acima ou abaixo
+
+        // Acima
+            if (strcmp(posicao,"acima")==0)
+            {
+                soma = somaAcima(indice,vetor);
+                // printf("\nteste acima\n");
+            }
+        
+        // Abaixo
+            else if(strcmp(posicao,"abaixo")==0)
+            {
+                soma = somaAbaixo(indice,vetor);
+                // printf("\nteste abaixo\n");
+            }
+
+        // Tratamento de Erro
+            else
+            {
+                printf("\nErro, digite um valor valido [acima ou abaixo]\n\n");
+            }
+        
+        // Verificando se soma é maior que limiar
+            if (soma>limiar)
+                {
+                    printf("Verdade");
+                }
+            else
+                {
+                    printf("Falso");
+                }
+    return 0;
+}
+
+// Funções
+
+void recebeVetor(int l,int vetor[l][l])
+{
+    for(int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            scanf("%d",&vetor[i][j]);
+        }
+    }
+}
+
+int somaAcima(int l,int vetor[l][l])
+{
+    int soma = 0;
+    for(int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            if(j>i)
+            {
+                soma += vetor[i][j];
+            }
+        }
+    }
+    return soma;
+}
+
+int somaAbaixo(int l,int vetor[l][l])
+{
+    int soma = 0;
+    for(int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            if(j<i)
+            {
+                soma += vetor[i][j];
+            }
+        }
+    }
+    return soma;
+}
+
+void imprimeVetor(int l,int vetor[l][l])
+{
+    for(int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            printf("%d ",vetor[i][j]);
+        }
+        printf("\n");
+    }
+}
